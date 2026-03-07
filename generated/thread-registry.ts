d1e80f3b45217619417461520cb4a4af95fcf8a4
SuggarCoder <gvoicelilu@gmail.com>
2026-03-07 04:46:58 -0800

    chore: remove thread page 1772722183-532409

--- generated/thread-registry.ts
+++ generated/thread-registry.ts
@@ -1,12 +1,6 @@
 import type { ThreadRegistryEntry } from "@/types/thread";
 
 export const threadRegistry: ThreadRegistryEntry[] = [
-  {
-    threadId: "1772722183-532409",
-    title: "M4 Pro / M5 Pro 价格监控系统",
-    updatedAt: "2026-03-05T14:53:13.853Z",
-    importPage: () => import("./threads/1772722183-532409/page")
-  },
 ];
 
 export const threadRegistryMap = Object.fromEntries(
